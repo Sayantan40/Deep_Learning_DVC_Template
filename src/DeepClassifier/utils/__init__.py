31aa23bcdaa007999def4479f81b271213fc9960
Sayantan40 <sayantanmitra.kol28@gmail.com>
2022-10-26 20:53:27 +0530

    deep import for Direct Import

---
+++ src/DeepClassifier/utils/__init__.py
@@ -0,0 +1,4 @@
+from DeepClassifier.utils.common import *
+
+## The Advantage of this is whenever we need any utility functions we dont need to write  .commom again
+## it is already been imported to the init.py file
